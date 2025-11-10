1e0a5180ba977934009e6dd132a6a5a52b3b5bab
KStädter <Konrad.staedter@gmail.com>
2025-11-10 13:59:21 +0100

    das Programm erfüllt zweck

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-from stats import count_words 
+from stats import count_words,sortierer
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as datei:
         text = datei.read()
     return text
 
-def count_characters(text):
-    text_lower=text.lower()
-    lexika={"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
-    for buchstabe in text_lower:
-        if buchstabe in lexika:
-            lexika[buchstabe]+=1
-    return lexika
-        
 
-
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    wortzahl = count_words(text)
-    print(f"Found {wortzahl} total words")
-    character_dict = count_characters(text)
-    print(character_dict)
     
-
-
-if __name__ == "__main__":
-    main()
-
-    """
-    python
 def count_characters(text: str) -> dict[str, int]:
     counts: dict[str, int] = {}
     for ch in text.lower():
         counts[ch] = counts.get(ch, 0) + 1
     return counts 
-    """
+    
+
+def main():
+    if len(sys.argv)!=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
+    wortzahl = count_words(text)
+    print(f"Found {wortzahl} total words")
+    character_dict = count_characters(text)
+    x=sortierer(character_dict)
+    print(x)
+    
+
+
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def sort_on(item):
+    return item["num"]
+
+def sortierer(character_dict):
+    directories_list=[]
+    for i in character_dict:
+        d={"char":i,"num":character_dict[i]}
+        directories_list.append(d)
+    key=sort_on(directories_list[1])
+    directories_list.sort(reverse=True, key=sort_on)
+
+    for i in range(0, len(directories_list)):
+        print(f"{directories_list[i]["char"]}: {directories_list[i]["num"]}")
+    x="erledigt"
+    return x
